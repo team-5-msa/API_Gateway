64b8e13089ef2151b62a8becee1f012f12a4c236
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-11-21 07:34:17 +0900

    Auth-verify + performance + payment 토큰 존재 여부 middleware 추가

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const authRoutes = createProxyMiddleware({
+    // target : http://localhost:3001/auth
+    // http://localhost:3002
     target: process.env.AUTH_SERVICE_URL,
     changeOrigin: true,
-    // pathRewrite: {
-    //     "^/auth": "",
-    // }
+    pathRewrite: function (path, req) {
+        // /auth/verify 요청을 그대로 유지
+        if (req.originalUrl.startsWith("/auth/verify")) {
+            return "/verify"; 
+        }
+
+        // /auth/signup + /auth/login은 /auth 제거
+        return path.replace(/^\/auth/, "");
+    }
+
 });
 
 module.exports = authRoutes;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { authRoutes, performanceRoutes, paymentRoutes } = require("./routes/routeModule");
+const { checkTokenExist } = require("./middlewares/tokenCheckMiddleware")
 
 const app = express();
 
-app.use("/auth", authRoutes);
-app.use("/performance", performanceRoutes);
-app.use("/payment", paymentRoutes);
+app.use("/auth/verify", checkTokenExist, authRoutes);
+app.use("/auth", authRoutes); // http://localhost:3001/auth
+app.use("/performance", checkTokenExist, performanceRoutes);
+app.use("/payment", checkTokenExist, paymentRoutes);
 
 app.use(express.json());
 app.use(cors({
