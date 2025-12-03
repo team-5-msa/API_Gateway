91540d3fe2aca5573aab6150c989d31075cb0975
JHParrrk <112451800+JHParrrk@users.noreply.github.com>
2025-12-03 18:41:02 +0900

    rollback

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const bookingRoutes = createProxyMiddleware({
   target: process.env.BOOKING_SERVICE_URL,
   changeOrigin: true,
-  pathRewrite: {
-    "^/booking": "",
-  },
+  //   pathRewrite: {
+  //     "^/booking": "",
+  //   },
 });
 
 module.exports = bookingRoutes;
